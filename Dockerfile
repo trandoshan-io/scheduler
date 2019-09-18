a973da18345471d5536daaf1ac39100d92c7de1a
Aloïs Micard <lunamicard@gmail.com>
2019-09-18 13:46:01 +0200

    Migrate from RabbitMQ to NATS

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,7 @@ RUN apk update && apk upgrade && \
     apk add --no-cache bash git openssh
 
 RUN go get -v github.com/joho/godotenv && \
-    go get -v github.com/streadway/amqp && \
-    go get -v github.com/trandoshan-io/amqp
+    go get -v github.com/nats-io/nats.go/
 
 COPY . /app/
 WORKDIR /app
--- scheduler.go
+++ scheduler.go
@@ -3,8 +3,7 @@ package main
 import (
    "encoding/json"
    "github.com/joho/godotenv"
-   "github.com/streadway/amqp"
-   tamqp "github.com/trandoshan-io/amqp"
+   "github.com/nats-io/nats.go"
    "log"
    "net/http"
    "os"
@@ -13,12 +12,11 @@ import (
 )
 
 const (
-   todoQueue = "todo"
-   doneQueue = "done"
+   crawlingQueue = "crawlingQueue"
+   todoSubject   = "todoSubject"
+   doneSubject   = "doneSubject"
 )
 
-//TODO: route message based on url ? like building a BTREE to dispatch message to crawler in a efficient manner
-// this will allow high performance when dealing with a lot of urls (will reduce check complexity)
 func main() {
    log.Println("Initializing scheduler")
 
@@ -42,59 +40,52 @@ func main() {
 
    log.Println("Loaded " + strconv.Itoa(len(forbiddenExtensions)) + " forbidden extensions from the API")
 
-   prefetch, err := strconv.Atoi(os.Getenv("AMQP_PREFETCH"))
+   // connect to NATS server
+   nc, err := nats.Connect(os.Getenv("NATS_URI"))
    if err != nil {
-      log.Fatal(err)
+      log.Fatal("Error while connecting to nats server: ", err)
+   }
+   defer nc.Close()
+
+   // initialize queue subscriber
+   if _, err := nc.QueueSubscribe(doneSubject, crawlingQueue, handleMessages(nc, forbiddenExtensions)); err != nil {
+      log.Fatal("Error while trying to subscribe to server: ", err)
    }
 
-   // initialize publishers
-   publisher, err := tamqp.NewStateFullPublisher(os.Getenv("AMQP_URI"))
-   if err != nil {
-      log.Fatal("Unable  to create publisher: ", err.Error())
-   }
-   log.Println("Publisher initialized successfully")
-
-   // initialize consumer & start him
-   consumer, err := tamqp.NewConsumer(os.Getenv("AMQP_URI"), prefetch)
-   if err != nil {
-      log.Fatal("Unable to create consumer: ", err.Error())
-   }
-   if err := consumer.Consume(doneQueue, false, handleMessages(publisher, forbiddenExtensions)); err != nil {
-      log.Fatal("Unable to consume message: ", err.Error())
-   }
    log.Println("Consumer initialized successfully")
 
    //TODO: better way
    select {}
-
-   _ = consumer.Shutdown()
 }
 
-func handleMessages(publisher tamqp.Publisher, forbiddenExtensions []string) func(deliveries <-chan amqp.Delivery, done chan error) {
-   return func(deliveries <-chan amqp.Delivery, done chan error) {
-      for delivery := range deliveries {
-         var url string
+func handleMessages(nc *nats.Conn, forbiddenExtensions []string) func(*nats.Msg) {
+   return func(msg *nats.Msg) {
+      var url string
 
-         // Unmarshal message
-         if err := json.Unmarshal(delivery.Body, &url); err != nil {
-            log.Println("Error while de-serializing payload: ", err.Error())
-            _ = delivery.Reject(false)
-            continue
+      // Unmarshal message
+      if err := json.Unmarshal(msg.Data, &url); err != nil {
+         log.Println("Error while de-serializing payload: ", err)
+         // todo: store in sort of DLQ?
+         return
+      }
+
+      // clean / sanitize url
+      cleanUrl := strings.TrimSuffix(url, "/")
+
+      // make sure url is not crawled
+      if shouldParse(cleanUrl, forbiddenExtensions) {
+         log.Println(url, " should be parsed")
+
+         // publish url in todo queue
+         bytes, err := json.Marshal(url)
+         if err != nil {
+            log.Println("Error while serializing message into json: ", err)
+            return
          }
-
-         // clean / sanitize url
-         cleanUrl := strings.TrimSuffix(url, "/")
-
-         // make sure url is not crawled
-         if shouldParse(cleanUrl, forbiddenExtensions) {
-            log.Println(url, " should be parsed")
-            if err := publisher.PublishJson("", todoQueue, url); err != nil {
-               log.Println("Error while trying to publish to done queue: ", err.Error())
-               _ = delivery.Reject(false)
-            }
+         if err = nc.Publish(todoSubject, bytes); err != nil {
+            log.Println("Error while trying to publish to done queue: ", err)
+            // todo: store in sort of DLQ?
          }
-
-         _ = delivery.Ack(false)
       }
    }
 }
@@ -125,7 +116,7 @@ func shouldParse(url string, forbiddenExtensions []string) bool {
 
    var body []map[string]interface{}
    if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
-      log.Println("Error while decoding json body. Assuming " + url + " has not been crawled: " + err.Error())
+      log.Println("Error while decoding json body. Assuming " + url + " has not been crawled: ", err)
       return true
    }
 
