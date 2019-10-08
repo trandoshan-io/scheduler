e1472678f16a12f84ed2514dd607af380bc00ce8
Aloïs Micard <lunamicard@gmail.com>
2019-10-08 12:27:37 +0200

    Project cleanup
    - Add LICENSE
    - Gofmt file
    - [#5] Complete readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # scheduler
-Go orchestrator process used to schedule website parsing
+
+[![Go Report Card](https://goreportcard.com/badge/github.com/trandoshan-io/scheduler)](https://goreportcard.com/report/github.com/trandoshan-io/scheduler)
+
+Scheduler is a Go written program designed to orchestrate resource parsing
+
+## features
+
+- use scalable messaging protocol (nats)
+
+## how it work
+
+- The Scheduler process connect to a nats server (specified by env variable *NATS_URI*) 
+and set-up a subscriber for message with tag *doneSubject*
+- When an URL is received the scheduler will apply list of *crawling rules* to determinate if resource is to be crawled
+- If resource should be crawled to scheduler will sent the url to nats with subject *todoSubject* for the crawlers
+
+## crawling rules
+
+Here is the rules that determinate if crawling is to be done:
+
+- Url has not been already crawled
--- scheduler.go
+++ scheduler.go
@@ -1,93 +1,93 @@
 package main
 
 import (
-   "encoding/json"
-   "github.com/nats-io/nats.go"
-   "log"
-   "net/http"
-   "os"
-   "strings"
+	"encoding/json"
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 const (
-   crawlingQueue = "crawlingQueue"
-   todoSubject   = "todoSubject"
-   doneSubject   = "doneSubject"
+	crawlingQueue = "crawlingQueue"
+	todoSubject   = "todoSubject"
+	doneSubject   = "doneSubject"
 )
 
 func main() {
-   log.Println("Initializing scheduler")
+	log.Print("Initializing scheduler")
 
-   // connect to NATS server
-   nc, err := nats.Connect(os.Getenv("NATS_URI"))
-   if err != nil {
-      log.Fatal("Error while connecting to nats server: ", err)
-   }
-   defer nc.Close()
+	// connect to NATS server
+	nc, err := nats.Connect(os.Getenv("NATS_URI"))
+	if err != nil {
+		log.Fatalf("Error while connecting to nats server: %s", err)
+	}
+	defer nc.Close()
 
-   // initialize queue subscriber
-   if _, err := nc.QueueSubscribe(doneSubject, crawlingQueue, handleMessages(nc)); err != nil {
-      log.Fatal("Error while trying to subscribe to server: ", err)
-   }
+	// initialize queue subscriber
+	if _, err := nc.QueueSubscribe(doneSubject, crawlingQueue, handleMessages(nc)); err != nil {
+		log.Fatalf("Error while trying to subscribe to server: %s", err)
+	}
 
-   log.Println("Consumer initialized successfully")
+	log.Print("Consumer initialized successfully")
 
-   //TODO: better way
-   select {}
+	//TODO: better way
+	select {}
 }
 
 func handleMessages(nc *nats.Conn) func(*nats.Msg) {
-   return func(msg *nats.Msg) {
-      var url string
+	return func(msg *nats.Msg) {
+		var url string
 
-      // Unmarshal message
-      if err := json.Unmarshal(msg.Data, &url); err != nil {
-         log.Println("Error while de-serializing payload: ", err)
-         // todo: store in sort of DLQ?
-         return
-      }
+		// Unmarshal message
+		if err := json.Unmarshal(msg.Data, &url); err != nil {
+			log.Printf("Error while de-serializing payload: %s", err)
+			// todo: store in sort of DLQ?
+			return
+		}
 
-      // clean / sanitize url
-      cleanUrl := strings.TrimSuffix(url, "/")
+		// clean / sanitize url
+		cleanUrl := strings.TrimSuffix(url, "/")
 
-      // make sure url is not crawled
-      if shouldParse(cleanUrl) {
-         log.Println(url, " should be parsed")
+		// make sure url is not crawled
+		if shouldParse(cleanUrl) {
+			log.Printf("%s should be parsed", url)
 
-         // publish url in todo queue
-         bytes, err := json.Marshal(url)
-         if err != nil {
-            log.Println("Error while serializing message into json: ", err)
-            return
-         }
-         if err = nc.Publish(todoSubject, bytes); err != nil {
-            log.Println("Error while trying to publish to done queue: ", err)
-            // todo: store in sort of DLQ?
-         }
-      }
-   }
+			// publish url in todo queue
+			bytes, err := json.Marshal(url)
+			if err != nil {
+				log.Printf("Error while serializing message into json: %s", err)
+				return
+			}
+			if err = nc.Publish(todoSubject, bytes); err != nil {
+				log.Printf("Error while trying to publish to done queue: %s", err)
+				// todo: store in sort of DLQ?
+			}
+		}
+	}
 }
 
 // check if url contains not invalid stuff and if not already crawled
 func shouldParse(url string) bool {
-   // make sure URL is not already managed
-   // this is done in the last part because heaviest operation
-   resp, err := http.Get(os.Getenv("API_URI") + "/pages?url=" + url)
-   if err != nil {
-      log.Println("Error while checking if url " + url + " has been crawled. Assuming not")
-      return true
-   }
+	// make sure URL is not already managed
+	resp, err := http.Get(fmt.Sprintf("%s/pages?url=%s", os.Getenv("API_URI"), url))
+	if err != nil {
+		log.Printf("Error while checking if url %s has been crawled. Assuming not", url)
+		return true
+	}
 
-   var body []map[string]interface{}
-   if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
-      log.Println("Error while decoding json body. Assuming " + url + " has not been crawled: ", err)
-      return true
-   }
+	var body []map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		log.Printf("Error while decoding json body. Assuming %s has not been crawled: %s", url, err)
+		return true
+	}
 
-   // result: url has been crawled
-   if len(body) > 0 {
-      return false
-   }
+	// result: url has been crawled
+	if len(body) > 0 {
+		return false
+	}
 
-   return true
+	return true
 }
